25d706e6de69fca81b598ccdfd5af651b9200105
kateincoding <kateincoding@gmail.com>
2021-06-13 23:11:42 +0000

    update h

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -24,5 +25,9 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif
