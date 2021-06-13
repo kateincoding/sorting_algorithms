5b507c6d26a8d599dda65b733ff549e91eac84ea
kateincoding <kateincoding@gmail.com>
2021-06-13 04:43:16 +0000

    bubble sort with betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,6 +27,9 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i = 0, j = 0;
 
+	if (size <= 1)
+		return;
+
 	for (i = 0; i < (size - 1); i++)
 		for (j = 1; j < size; j++)
 			if (array[j - 1] > array[j])
