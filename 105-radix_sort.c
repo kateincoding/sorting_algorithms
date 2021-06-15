262053f0f046707034b1f361297a62f8f20e9062
kateincoding <kateincoding@gmail.com>
2021-06-15 05:36:35 +0000

    return to malloc

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -5,16 +5,20 @@
  * the number of do arecursion is linear depend with
  * the #digits of max number
  * @array: unsorted array of integer
+ * @result: possible result
+ * @index: index table where we have the counting numbers of the array
  * @size: size of the array
  * @lsd: least significant digit (1 last: 1, 2nd last: 1*10, 3: 1*10*10)
  *
  * Return: void
  */
-void recursive_radix_sort(int *array, size_t size, int lsd)
+void recursive_radix_sort(int *array, int *result,
+						int *index, size_t size, int lsd)
 {
-	int index[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int result[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	int i, idx_start;
+	int i, idx_start, idx_index;
+
+	for (i = 0; i < (int)size; i++)
+		index[i] = 0;
 
 	/* count the repetitions of digits in nbr and store in index */
 	for (i = 0; i < (int)size; i++)
@@ -23,10 +27,7 @@ void recursive_radix_sort(int *array, size_t size, int lsd)
 	/* The final of recursion is when the position-digit of all nbr == 0 */
 	if (index[0] == (int)size)
 		return;
-	
-	// result = malloc(sizeof(int) * size);
-	// if (!result)
-	// 	return;
+
 	/* we can work in here building a hash table or hash array */
 	/* or work like insertion, let's calculate the index and set */
 	for (i = 1; i < 10; i++)
@@ -34,29 +35,24 @@ void recursive_radix_sort(int *array, size_t size, int lsd)
 
 	for (i = 9; i > 0; i--)
 		index[i] = index[i - 1];
-	index[0] = [0];
+	index[0] = 0;
 
 	/* now let's have a middle sorted array */
-	// for (i = (int)size - 1; i >= 0; i--)
 	for (i = 0; i < (int)size; i++)
 	{
 		/* check the start position of array[i] with index table */
 		idx_index = (array[i] / lsd) % 10;
-		if (idx_index > 0)
-			idx_start = index[idx_index - 1];
-		else 
-			idx_start = index[0];
+		idx_start = index[idx_index];
 		/* assign in the partial sorted array */
 		result[idx_start] = array[i];
 		/* final position decrease 1 because we already assign the value */
-		index[idx_index - 1]++;
+		index[idx_index]++;
 	}
-	
+	/* copy the result to the array and print it */
 	for (i = 0; i < (int)size; i++)
 		array[i] = result[i];
 	print_array(array, size);
-	/* index -> partial sorted array */
-	recursive_radix_sort(array, size, lsd * 10);
+	recursive_radix_sort(array, result, index, size, lsd * 10);
 }
 
 /**
@@ -73,10 +69,22 @@ void recursive_radix_sort(int *array, size_t size, int lsd)
  */
 void radix_sort(int *array, size_t size)
 {
-	if (size <= 1)
-		return;
+	int *result, *index_table;
 
+	if (!array || size <= 1)
+		return;
+	result = malloc(sizeof(int) * size);
+	if (!result)
+		return;
+	index_table = malloc(sizeof(int) * 10);
+	if (!index_table)
+	{
+		free(result);
+		return;
+	}
 	/* one way, we will find how many time sort = #digits of max_number */
-	/* Second way, build a recursive fx but it is helpfull if the array is bigger */
-	recursive_radix_sort(array, size, 1);
+	/* Second way,recursion it is helpfull if the array is bigger */
+	recursive_radix_sort(array, result, index_table, size, 1);
+	free(result);
+	free(index_table);
 }
