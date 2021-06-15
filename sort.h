6cb6068504c6e9933421821643b12bdb21cd4653
kateincoding <kateincoding@gmail.com>
2021-06-15 05:13:34 +0000

    adding fx

--- sort.h
+++ sort.h
@@ -29,5 +29,9 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
